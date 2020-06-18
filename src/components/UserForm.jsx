42744da098f2fc9c5c1f4a9fc3129b5ec4b3165f
j-dublon <j.johnson.dublon@gmail.com>
2020-06-18 09:48:44 +0100

    add axios API call

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,65 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
 import UserCard from "./UserCard";
+import axios from "axios";
 
-const UserForm = () => {
-  const users = [
-    { name: "John Smith", email: "john@smith.com", company: "Google" },
-    { name: "Mark Smithy", email: "mark@smithy.com", company: "Amazon" },
-    { name: "Alex Smithson", email: "alex@smithson.com", company: "Facebook" },
-  ];
+class UserForm extends Component {
+  state = {
+    users: [],
+  };
 
-  return (
-    <main>
-      <form className="userForm__form">
-        <h2 className="userForm__label">Filter users:</h2>
-        <input
-          type="text"
-          name="name"
-          placeholder="By name..."
-          className="userForm__field"
-        ></input>
-        <input
-          type="text"
-          name="email"
-          placeholder="By email..."
-          className="userForm__field"
-        ></input>
-        <input
-          type="text"
-          name="company"
-          placeholder="By company..."
-          className="userForm__field"
-        ></input>
-      </form>
-      <section className="userCard__container">
-        {users.map((user) => {
-          return <UserCard key={user.name} {...user} />;
-        })}
-      </section>
-      <form className="userForm__form">
-        <h2 className="userForm__label">Add new user:</h2>
-        <input
-          type="text"
-          name="addName"
-          placeholder="Name..."
-          className="userForm__field"
-        ></input>
-        <input
-          type="text"
-          name="addEmail"
-          placeholder="Email..."
-          className="userForm__field"
-        ></input>
-        <input
-          type="text"
-          name="addCompany"
-          placeholder="Company..."
-          className="userForm__field"
-        ></input>
-        <button className="userForm__submit">Submit</button>
-      </form>
-    </main>
-  );
-};
+  componentDidMount() {
+    axios.get("https://renewable.exchange/test/users.json").then((response) => {
+      this.setState({ users: response.data });
+    });
+  }
+
+  render() {
+    return (
+      <main>
+        <form className="userForm__form">
+          <h2 className="userForm__label">Filter users:</h2>
+          <input
+            type="text"
+            name="name"
+            placeholder="By name..."
+            className="userForm__field"
+          ></input>
+          <input
+            type="text"
+            name="email"
+            placeholder="By email..."
+            className="userForm__field"
+          ></input>
+          <input
+            type="text"
+            name="company"
+            placeholder="By company..."
+            className="userForm__field"
+          ></input>
+        </form>
+        <section className="userCard__container">
+          {this.state.users.map((user) => {
+            return <UserCard key={user.name} {...user} />;
+          })}
+        </section>
+        <form className="userForm__form">
+          <h2 className="userForm__label">Add new user:</h2>
+          <input
+            type="text"
+            name="addName"
+            placeholder="Name..."
+            className="userForm__field"
+          ></input>
+          <input
+            type="text"
+            name="addEmail"
+            placeholder="Email..."
+            className="userForm__field"
+          ></input>
+          <input
+            type="text"
+            name="addCompany"
+            placeholder="Company..."
+            className="userForm__field"
+          ></input>
+          <button className="userForm__submit">Submit</button>
+        </form>
+      </main>
+    );
+  }
+}
 
 export default UserForm;
