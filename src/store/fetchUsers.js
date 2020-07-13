378692b1ae41aa9afc475913232e327d8dbd90cd
j-dublon <j.johnson.dublon@gmail.com>
2020-07-13 12:25:08 +0100

    reconfigure fetchUsers to use axios

--- src/store/fetchUsers.js
+++ src/store/fetchUsers.js
@@ -3,22 +3,30 @@ import {
   fetchUsersSuccess,
   fetchUsersError,
 } from "./actions";
+import axios from "axios";
 const config = require("../config.json");
 
 export function fetchUsers() {
   return (dispatch) => {
     dispatch(fetchUsersPending());
-    return fetch(`${config.api.invokeUrl}/users`)
-      .then((res) => res.json())
-      .then((json) => {
-        if (json.error) {
-          throw json.error;
-        }
-        dispatch(fetchUsersSuccess(json));
-        return json;
+    return axios
+      .get(`${config.api.invokeUrl}/users`)
+      .then((res) => {
+        dispatch(fetchUsersSuccess(res.data));
+        return res.data;
       })
       .catch((error) => {
         dispatch(fetchUsersError(error));
       });
+    // .then((json) => {
+    //   if (json.error) {
+    //     throw json.error;
+    //   }
+    //   dispatch(fetchUsersSuccess(json));
+    //   return json;
+    // })
+    // .catch((error) => {
+    //   dispatch(fetchUsersError(error));
+    // });
   };
 }
